0eb326869adb6978cdfd73b949431cb59b94a6f7
KOMIYAMA Akiko <21716041@jwu.ac.jp>
2019-10-07 16:33:52 +0900

    rational class

--- rational.py
+++ rational.py
@@ -5,8 +5,18 @@ class Q(object):
         self.b = b
 
     def __repr__(self):
+        if self.b == 1:
+            return str(self.a)
         return f'{self.a}/ {self.b}'
+    def __add__(self, q):
+        a = self.a
+        b = self.b
+        c = q.a
+        d = q.b
+        return Q(a*d+b*c, b*d)
 
-q = Q(1,2)
-print(q)
+
+q1 = Q(1,2)
+q2 = Q(1,3)
+print(q1 + q2)
 
