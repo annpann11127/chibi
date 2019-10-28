2ed9e64bdae03d4b9018732fe1477cc00832ff1b
KOMIYAMA Akiko <21716041@jwu.ac.jp>
2019-10-28 16:21:51 +0900

    output binary Add

--- parser.py
+++ parser.py
@@ -1,17 +1,31 @@
-from exp import Val, Add
+from exp import Val, Add, Sub, Mul, Div
 
+'''
 def parse(s: str):
     num = int(s)
     return Val(num)
 
-e = parse("123")
+e = parse("1+2")
 print(e)
 
-s = "1+2"
+s = "123+456"
 pos = s.find('+')
 print('pos', pos)
 
 s1 = s[0:pos]
 s2 = s[pos+1:]
-print(s, s1, s2)
+print(s, s1, s2
+'''
+
+def parse(s:str):
+    pos = s.find('+')
+    if pos == -1:
+        return Val(int(s))
+    else:
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Add(parse(s1), parse(s2))
+
+e = parse("123+456+789")
+print(e)
 
