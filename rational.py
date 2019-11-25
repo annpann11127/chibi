57ce8f3387d7874bdf75d2596c11a59a85e9dc15
KOMIYAMA Akiko <21716041@jwu.ac.jp>
2019-11-25 15:53:24 +0900

    rationl commit

--- rational.py
+++ rational.py
@@ -1,9 +1,11 @@
+import math
+
 class Q(object):
 
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
-
+    def __init__(self, a, b=1):
+        gcd = math.gcd(a,b)
+        self.a = a//gcd
+        self.b = b//gcd
     def __repr__(self):
         if self.b == 1:
             return str(self.a)
@@ -34,13 +36,10 @@ class Q(object):
         return Q(a*d, b*c)
 
 
-
-
-
 q1 = Q(1,2)
 q2 = Q(1,3)
 print(q1 + q2)
-print(q1 - q2)
-print(q1 * q2)
-print(q1 / q2)
+#print(q1 - q2)
+#print(q1 * q2)
+#print(q1 / q2)
 
