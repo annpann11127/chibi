876b27de7b99e4520f80250dc8b05e9734d50b34
KOMIYAMA Akiko <21716041@jwu.ac.jp>
2019-12-09 15:47:49 +0900

    Eq Ne Lt

--- chibi.py
+++ chibi.py
@@ -60,6 +60,23 @@ class Mod(Binary):
     def eval(self, env: dict):
         return self.left.eval(env) % self.right.eval(env)
 
+class Eq(Binary):
+    __slots__ = ['left', 'right']
+    def eval(self, env: dict):
+        return 1 if self.left.eval(env) == self.right.eval(env) else 0
+
+class Ne(Binary):
+     __slots__ = ['left', 'right']
+    def eval(self, env: dict):
+        return 1 if self.left.eval(env) != self.right.eval(env) else 0
+
+class Lt(Binary):
+    __slots__ = ['left', 'right']
+    def eval(self, env: dict):
+        return 1 if self.left.eval(env) < self.right.eval(env) else 0
+    
+
+
 class Var(Expr):
     __slots__ = ['name']
     def __init__(self, name):
